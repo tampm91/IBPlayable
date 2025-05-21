a61787650ad448bfef92c06b1b9392c64dfc7578
tampm91 <69116612+tampm91@users.noreply.github.com>
2025-05-21 09:42:46 +0700

    test PA
    Upload first version

---
+++ src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["point","handImpactPoint"],collisionMatrix:[[false,true],[true,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/game.fire",orientation:"portrait",jsList:[]};
